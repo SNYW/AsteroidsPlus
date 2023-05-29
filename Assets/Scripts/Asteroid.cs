03150eb48a15289435d77d2c3581eb574f972000
Sonny Williams <sonnywilliams@hotmail.co.uk>
2023-05-29 13:02:42 +0100

    Adjusting Asteroid Randomisation

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -2,48 +2,78 @@ using System;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Asteroid : MonoBehaviour
 {
 
    [SerializeField] private RangeFloat minMaxRadius;
-   [SerializeField] private RangeFloat minMaxPointOffset;
-   [SerializeField] private RangeFloat minMaxOffset;
+   [SerializeField] private RangeFloat minMaxPoints;
+   [SerializeField] private RangeFloat minMaxPosOffset;
+   [SerializeField] private RangeFloat minMaxDistanceOffset;
+   [SerializeField] private RangeFloat minMaxInitSpeed;
+   [SerializeField] private RangeFloat minMaxInitTorque;
    
    private LineRenderer _lr;
    private PolygonCollider2D _pc;
+   private Rigidbody2D _rb;
+   private Vector2 startPos;
 
    private void Awake()
    {
       _lr = GetComponentInChildren<LineRenderer>();
       _pc = GetComponent<PolygonCollider2D>();
+      _rb = GetComponent<Rigidbody2D>();
+      startPos = transform.position;
+
+      Initialize();
+   }
+
+   private void Initialize()
+   {
       GenerateAsteroid();
+      InitCollider();
+      InitForces();
+   }
+
+   private void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.G))
+      {
+         Initialize();
+      }
    }
 
    private void GenerateAsteroid()
    {
+      transform.position = startPos;
+      _rb.velocity = Vector2.zero;
+      _rb.angularVelocity = 0;
+      
       var radius = minMaxRadius.RandomValue();
-      var pointAmount = radius + minMaxPointOffset.RandomValue();
+      var pointAmount = minMaxPoints.RandomValue();
 
       _lr.positionCount = (int)pointAmount;
 
-      var angleOffset = 2 * Mathf.PI / pointAmount;
+      var angleOffset = 2 * Mathf.PI / _lr.positionCount;
       var angle = 0f;
 
-      for (int i = 0; i < pointAmount-1; i++)
+      for (int i = 0; i < _lr.positionCount; i++)
       {
-         
+
          Vector2 pointPos = new Vector2(
-            Mathf.Cos(angle)*radius + minMaxOffset.RandomValue(), 
-            Mathf.Sin(angle)*radius + minMaxOffset.RandomValue()
+            Mathf.Cos(angle)*radius + minMaxPosOffset.RandomValue(), 
+            Mathf.Sin(angle)*radius + minMaxPosOffset.RandomValue()
          );
 
-         _lr.SetPosition(i, pointPos);
+         var distanceOffset = ((Vector2)startPos - pointPos).normalized * minMaxDistanceOffset.RandomValue();
+
+         _lr.SetPosition(i, pointPos+distanceOffset);
          
          angle += angleOffset;
       }
 
-      InitCollider();
+      _rb.mass = radius / 10;
    }
 
    private void InitCollider()
@@ -53,4 +83,16 @@ public class Asteroid : MonoBehaviour
       
       _pc.SetPath(0, positions.Select(v => new Vector2(v.x, v.y)).ToArray());
    }
+
+   private void InitForces()
+   {
+      var dirToCentre = Vector2.zero - (Vector2)transform.position;
+
+      _rb.AddForce(dirToCentre * (_rb.mass * minMaxInitSpeed.RandomValue()), ForceMode2D.Force);
+      var balancedRange = new RangeFloat(
+         Random.Range(minMaxInitTorque.min, minMaxInitTorque.min / 2),
+         Random.Range(minMaxInitTorque.max, minMaxInitTorque.max / 2));
+      _rb.AddTorque(balancedRange.RandomValue()*_rb.mass, ForceMode2D.Impulse);
+   }
+   
 }
